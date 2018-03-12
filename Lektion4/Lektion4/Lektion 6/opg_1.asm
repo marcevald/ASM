5ac4855312d306dbc570a91ee33cfa5e1e716ad7
Marc <marc.evald@gmail.com>
2018-03-12 16:53:46 +0100

    Lektion 6 opg_1 updated, not finished

--- Lektion4/Lektion4/Lektion 6/opg_1.asm
+++ Lektion4/Lektion4/Lektion 6/opg_1.asm
@@ -22,11 +22,11 @@ init:
 	.equ d = 0b11111101
 	.equ e = 0b11111110
 	.equ f = 0b11101111
+	.equ ze = 0b00000000
 
 	.def count = R24 ;Count register
-	.def deb = R25 ;Debounce register
-
 	.def delay = R23 ;Delay time register
+
 
 	;Setup PORTC
 	ldi R16, 0x00 
@@ -36,28 +36,82 @@ init:
 
 	;Setup PORTB
 	out DDRB, R16 ; Set PORTB as output
-	ldi R16, a
-	out PORTB, R16 ;Turn on segment a
+	
 
-	
-	
+	; Infinte loop 
+
+	;LOOP:
+	;NOP
+	;BRNE LOOP
+
+	;Eventuelt iJMP
+	;Start med at tjek switches
+	;Ijmp kør loop med tidsforsinkelse afhængig af switches.
 
 main:
 
+debounce:
+	in R16, PINC ;Load PORTC to R16
+	com R16 ;Complement R16
+	rcall time2
+	in R17, PINC ;After delay load PORTC to R17
+	com R17
+	cp R16, R17 ;Compare PORTC before and after delay
+	brne debounce ;If not the same do again
 
+	mov delay, R16 ;Set delay time to switch input.
 
+test:
+sega:
+	ldi R16, a ;Load output value of segment
+	out PORTB, R16 ;Output value to PORTB
+	rcall time
+segb:
+	ldi R16, b ;Load output value of segment
+	out PORTB, R16 ;Output value to PORTB
+	rcall time
+segc:
+	ldi R16, c ;Load output value of segment
+	out PORTB, R16 ;Output value to PORTB
+	rcall time
+segd:
+	ldi R16, d ;Load output value of segment
+	out PORTB, R16 ;Output value to PORTB
+	rcall time
+sege:
+	ldi R16, e ;Load output value of segment
+	out PORTB, R16 ;Output value to PORTB
+	rcall time
+segf:
+	ldi R16, f ;Load output value of segment
+	out PORTB, R16 ;Output value to PORTB
+	rcall time
 
-
-
+	rjmp main
 
 
 
 
 ;-----DELAY SUBROUTINE-------	
-delay: 
-	mov R16, delay
-again:
-	NOP
-	dec R16
-	brne again
-	ret ;return to where delay was called.
+
+time:
+
+mov R20, delay
+LOOP1:
+ldi R21, 250
+LOOP2:
+DEC R21
+BRNE LOOP2
+DEC R20
+BRNE LOOP1
+ret
+
+;-------Delay 2 subroutine------------
+
+time2:
+
+ldi R21, 250
+LOOP:
+DEC R21
+BRNE LOOP
+ret
